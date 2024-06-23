9357244c2c93dc4dd832d3102ff75ebd8c9decc7
Matheus Kamer <matheus_kamer@hotmail.com>
2024-06-23 11:10:52 -0400

    chore: exchange url database

--- prisma/migrations/20231105185211_change_ondelete_in_users_relationship/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "bank_account" DROP CONSTRAINT "bank_account_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20240623150917_add_initial_tables/migration.sql
+++ prisma/migrations/20240623150917_add_initial_tables/migration.sql
@@ -1,5 +1,30 @@
 -- CreateEnum
+CREATE TYPE "bank_account_type" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- CreateEnum
 CREATE TYPE "transaction_type" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bank_account" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "bank_account_type" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_account_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "category" (
@@ -25,6 +50,12 @@ CREATE TABLE "transaction" (
 
     CONSTRAINT "transaction_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "category" ADD CONSTRAINT "category_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
