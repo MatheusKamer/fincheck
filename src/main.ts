920f09adb2f33c678a45211a89b2ea91c3df06a9
Matheus Kamer <matheus_kamer@hotmail.com>
2025-02-06 18:47:54 -0400

    feat: update server initialization to use ExpressAdapter and configure CORS settings

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import 'dotenv/config';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: '*',
+    origin: 'https://fincheck-frontend-92rs.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
-
-  await app.listen(3000);
+  await app.init();
 }
 
 bootstrap();
+
+export default server;
